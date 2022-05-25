e38dbe5c479e6b1865861dbab91aff143aa5ef63
Panditya A. Putra <me@panditya.com>
2022-05-25 19:43:35 +0700

    Add user controller

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\User\StoreRequest;
+use App\Http\Requests\Admin\User\UpdateRequest;
+use App\Http\Resources\Admin\UserResource;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class UserController extends Controller
+{
+    public function index(Request $request): AnonymousResourceCollection
+    {
+        $users = User::all();
+
+        return UserResource::collection($users);
+    }
+
+    public function store(StoreRequest $request): JsonResponse
+    {
+        User::create($request->all());
+
+        return new JsonResponse(null, 201);
+    }
+
+    public function show(User $user): UserResource
+    {
+        return new UserResource($user);
+    }
+
+    public function update(UpdateRequest $request, User $user): UserResource
+    {
+        $user->update($request->all());
+
+        return new UserResource($user);
+    }
+
+    public function destroy(User $user): JsonResponse
+    {
+        $user->delete();
+
+        return new JsonResponse(null, 204);
+    }
+}
