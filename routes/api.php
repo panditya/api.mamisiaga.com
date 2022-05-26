7399ca27a129c467278394fca874d32b68183803
Panditya A. Putra <me@panditya.com>
2022-05-26 14:22:54 +0700

    Add anthropometry import routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\AnthropometryImportController;
+use App\Http\Controllers\AnthropometryStandardImportController;
+use App\Http\Controllers\AnthropometryStatusImportController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResources([
     'users' => UserController::class
 ]);
+
+Route::prefix('anthropometries')->group(function () {
+    Route::post('import', AnthropometryImportController::class);
+    Route::post('statuses/import', AnthropometryStatusImportController::class);
+    Route::post('standards/import', AnthropometryStandardImportController::class);
+});
