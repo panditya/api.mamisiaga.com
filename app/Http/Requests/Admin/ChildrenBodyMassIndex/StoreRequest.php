3bbb78fe7b84eaa8dd4224808fb7dace9ed5b8d7
Panditya A. Putra <me@panditya.com>
2022-05-31 22:05:23 +0700

    Add children bmi resouce endpoints

---
+++ app/Http/Requests/Admin/ChildrenBodyMassIndex/StoreRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\Admin\ChildrenBodyMassIndex;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'age_in_months' => ['required', 'integer', 'min:0'],
+            'height_in_cm' => ['required', 'integer', 'min:0'],
+            'weight_in_kg' => ['required', 'integer', 'min:0'],
+            'head_circumference_in_cm' => ['required', 'integer', 'min:0'],
+        ];
+    }
+}
